cebc9562a9923008c4c9b68ca1cc05960c34b6eb
Eduardo Fazolo <dudu-fv@live.com>
2019-01-30 21:11:34 -0200

    codes reply message prettified

--- app/commands/codes.js
+++ app/commands/codes.js
@@ -9,8 +9,8 @@ module.exports.run = async (client, message, args) => {
     if(message.channel.name.toLowerCase() !== 'news-and-resources') return;
     let codes = await request_codes();
     let silos = ['Alpha', 'Beta', 'Charlie'];
-    codes = codes.map( (code,i) => "Silo " + silos[i] + ":" + code + "\n");
-    message.reply("The codes are: " + codes);
+    codes = codes.map( (code,i) => "**Silo " + silos[i] + ":** " + code + "\n");
+    message.reply("The codes are:\n" + codes);
 }
 
 module.exports.help = {
